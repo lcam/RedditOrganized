9a2b6ee471ee12c6d29700e916ff6f7c2372a459
lcam <leonzcamz101@gmail.com>
2019-10-17 00:47:37 -0700

    Cleaned up AuthViewModel and Network directory for review only

--- app/src/main/java/lcam/redditorganized/network/auth/AuthApi.java
+++ app/src/main/java/lcam/redditorganized/network/auth/AuthApi.java
@@ -2,22 +2,12 @@ package lcam.redditorganized.network.auth;
 
 import io.reactivex.Flowable;
 import lcam.redditorganized.models.OAuthToken;
-import lcam.redditorganized.models.User;
 import lcam.redditorganized.util.Constants;
-import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 public interface AuthApi {
-
-    // @Path would append a / to url
-    @GET("users/{id}")
-    Flowable<User> getUser(
-            @Path("id") int id
-    );
 
     @FormUrlEncoded
     @POST(Constants.ACCESS_TOKEN_URL)
--- app/src/main/java/lcam/redditorganized/ui/auth/AuthViewModel.java
+++ app/src/main/java/lcam/redditorganized/ui/auth/AuthViewModel.java
@@ -2,19 +2,13 @@ package lcam.redditorganized.ui.auth;
 
 import android.util.Log;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.LiveDataReactiveStreams;
 import androidx.lifecycle.ViewModel;
 
 import javax.inject.Inject;
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
-import io.reactivex.schedulers.Schedulers;
-import lcam.redditorganized.base.SessionManager;
 import lcam.redditorganized.models.OAuthToken;
-import lcam.redditorganized.models.User;
 import lcam.redditorganized.network.auth.AuthApi;
 import lcam.redditorganized.network.auth.AuthenticateUser;
 
@@ -24,57 +18,13 @@ public class AuthViewModel extends ViewModel {
 
     //inject
     private final AuthApi authApi;
-    private SessionManager sessionManager;
     private AuthenticateUser authenticateUser;
 
     @Inject
-    public AuthViewModel(AuthApi authApi, SessionManager sessionManager, AuthenticateUser authenticateUser) {
+    public AuthViewModel(AuthApi authApi, AuthenticateUser authenticateUser) {
         this.authApi = authApi;
-        this.sessionManager = sessionManager;
         this.authenticateUser = authenticateUser;
     }
-
-    public void authenticateWithId(int userId){
-        Log.d(TAG, "authenticateWithId: attempting to login");
-        sessionManager.authenticateWithId(queryUserId(userId));
-    }
-
-    private LiveData<AuthResource<User>> queryUserId(int userId){
-        //converting Flowable to LiveData obj, doing the api call
-        return LiveDataReactiveStreams.fromPublisher(
-                authApi.getUser(userId)
-
-                        //instead of calling onError (error happens)
-                        .onErrorReturn(new Function<Throwable, User>() {
-                            @Override
-                            public User apply(Throwable throwable) throws Exception {
-                                User errorUser = new User();
-                                errorUser.setId(-1);
-                                return errorUser;
-                            }
-                        })
-
-                        //wrap User object in AuthResource
-                        .map(new Function<User, AuthResource<User>>() {
-                            @Override
-                            public AuthResource<User> apply(User user) throws Exception {
-                                if(user.getId() == -1){
-                                    return AuthResource.error("Could not authenticate", (User)null);
-                                }
-                                return AuthResource.authenticated(user); //no error
-                            }
-                        })
-
-                        .subscribeOn(Schedulers.io()) //subscribe on a background thread
-        );
-    }
-
-    //observe MediatorLiveData from the UI
-    public LiveData<AuthResource<User>> observeAuthState(){
-        return sessionManager.getAuthUser(); //observe this LiveData obj, any changes made to that obj will get updated to UI
-                         //in this case, the only time it will get changed is if we successfully authenticate
-    }
-
 
     public void getAccessToken(String code) {
         Log.d(TAG, "Inside getAccessToken!!!");
